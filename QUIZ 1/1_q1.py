141a0f682597db025f6a59898b865c820b3d056f
Shristy Sharma <121787965+ShristySharma@users.noreply.github.com>
2023-01-15 16:35:18 +0530

    Add files via upload

---
+++ QUIZ 1/1_q1.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+import numpy as np
+A = [1,2]
+B = [7,0]
+plt.plot(A,B,marker='o')
+plt.show()
+
---
+++ QUIZ 1/1_q3.py
@@ -0,0 +1,5 @@
+import numpy as 
+A =np.array([[-5], [7]])
+B = np.array([[2],[0]])
+X = A-B
+print(np.linalg.norm(X))
