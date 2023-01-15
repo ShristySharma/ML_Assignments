0b1cc91499b0e1f6d532b885ade6ffa471cad704
Shristy Sharma <121787965+ShristySharma@users.noreply.github.com>
2023-01-15 17:43:29 +0530

    Add files via upload

---
+++ QUIZ_ASSIGNMENTS/QUIZ 4/q4_2.py
@@ -0,0 +1,13 @@
+import numpy as np
+A= np.array([[-2],[-1]])
+B= np.array([[4],[0]])
+C= np.array([[3],[3]])
+D= np.array([[-3],[2]])
+a=A-B
+b=D-C
+c=A-D
+d=B-C
+if (np.array_equal(a,b) and  np.array_equal(c,d)):
+    print("Since both sides of quadrilateral are parallel to each other.It is a Parallelogram")
+else:
+    print("it is not a parallelogram")
