c82ed447c00a408b8bda697feae154ad21f76318
Shristy Sharma <121787965+ShristySharma@users.noreply.github.com>
2023-01-20 15:56:50 +0530

    Add files via upload

--- 11.10.1.5/codes/11.10.1.5.py
+++ 11.10.1.5/codes/11.10.1.5.py
@@ -1,7 +1,31 @@
-import matplotlib.pyplot as plt
 import numpy as np
-A =np.array([[0],[-4]])
-B =np.array([[8],[0]])
-Q=(A+B)/2
-c= Q
-print(c)
+from matplotlib import pyplot as plt
+
+#Generate line points
+def line_gen(P,B):
+  len = 10
+  dim = P.shape[0]
+  x_PB = np.zeros((dim,len))
+  lam_1 = np.linspace(0,1,len)
+  for i in range(len):
+    temp1 = P + lam_1[i]*(B-P)
+    x_PB[:,i]= temp1.T
+  return x_PB
+
+
+P = np.array([[0.0],[-4.0]])
+B = np.array([[8.0],[0.0]])
+O = np.array([[0.0],[0.0]])
+C = line_gen(P, B)
+Q = (P + B)/2
+D = line_gen(Q, O)
+plt.text(P[0][0], P[1][0],'P')  
+plt.text(B[0][0],B[1][0], 'B')  
+plt.text(O[0][0],O[1][0], 'O') 
+plt.text(Q[0][0],Q[1][0], 'Q')  
+plt.plot(C[0], C[1])
+plt.plot(D[0], D[1])
+plt.grid()
+plt.tight_layout()
+plt.savefig('fig_1.png')
+                          
