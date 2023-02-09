484fe0cb2e48a8535989ef04850dd7dd1ce4bd6a
Shristy Sharma <121787965+ShristySharma@users.noreply.github.com>
2023-02-09 13:09:56 +0530

    Add files via upload

--- 12.11.3.8/codes/12.11.3.8.py
+++ 12.11.3.8/codes/12.11.3.8.py
@@ -1,7 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
-
+## computation for c
+n= np.array([[0],[1],[0]])
+x= np.array([[0],[3],[0]])
+c= n[0]*x[0] + n[1]*x[1] + n[2]*x[2]
+print(c)
 def plot_plane(y_intercept, color):
     x = np.linspace(-5, 5, 50)
     z = np.linspace(-5, 5, 50)
@@ -20,5 +24,5 @@ ax.set_zlabel('Z')
 ax.set_xlim(-5, 5)
 ax.set_ylim(-5, 5)
 ax.set_zlim(-5, 5)
-
 plt.show()
+
