b419361e38459c1f2fcc6ec6df385cfac83d0e36
Shristy Sharma <121787965+ShristySharma@users.noreply.github.com>
2023-03-01 09:14:01 +0530

    Add files via upload

--- 12.11.3.6/codes/12.11.3.6a.py
+++ 12.11.3.6/codes/12.11.3.6a.py
@@ -8,20 +8,20 @@ from line.funcs import *
 from triangle.funcs import *
 from conics.funcs import *
 
-p1= np.array([1,1,-1])
-p2= np.array([6,4,-5])
-p3= np.array([-4,-2,3])
+A= np.array([1,1,-1])
+B= np.array([6,4,-5])
+C= np.array([-4,-2,3])
 
-m=p2-p3
+m=B-C
 print('direction vector(m) =', m)
 # Calculate the normal vector of the plane
-v1 = p3 - p1
-v2 = p2 - p1
+v1 = C - A
+v2 = B - A
 normal = np.cross(v1, v2)
-print("x = {} + \u03BB{}".format(p1, m))
+print("x = {} + \u03BB{}".format(A, m))
 # Define the plane function
 def plane(x, y):
-    return (-normal[0] * x - normal[1] * y - np.dot(normal, p1)) * 1. / normal[2]
+    return (-normal[0] * x - normal[1] * y - np.dot(normal, A)) * 1. / normal[2]
 
 # Generate the meshgrid
 xx, yy = np.meshgrid(range(-10, 11), range(-10, 11))
@@ -32,12 +32,12 @@ z = plane(xx, yy)
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 ax.plot_surface(xx, yy, z)
-ax.scatter(p1[0], p1[1], p1[2], color='red')
-ax.text(p1[0],p1[1],p1[2], 'A')
-ax.scatter(p2[0], p2[1], p2[2], color='red')
-ax.text(p2[0],p2[1],p2[2], 'B')
-ax.scatter(p3[0], p3[1], p3[2], color='red')
-ax.text(p3[0],p3[1],p3[2], 'C')
-x_p3p2=line_gen(p3,p2)
-plt.plot(x_p3p2[0],x_p3p2[1],x_p3p2[2])
+ax.scatter(A[0], A[1], A[2], color='red')
+ax.text(A[0],A[1],A[2], 'A')
+ax.scatter(B[0], B[1], B[2], color='red')
+ax.text(B[0],B[1],B[2], 'B')
+ax.scatter(C[0], C[1], C[2], color='red')
+ax.text(C[0],C[1],C[2], 'C')
+x_CB=line_gen(C,B)
+plt.plot(x_CB[0],x_CB[1],x_CB[2])
 plt.show()
--- 12.11.3.6/codes/12.11.3.6b.py
+++ 12.11.3.6/codes/12.11.3.6b.py
@@ -3,31 +3,31 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
 
-p1= np.array([1,1,0])
-p2= np.array([1,2,1])
-p3= np.array([-2,2,-1])
+A= np.array([1,1,0])
+B= np.array([1,2,1])
+C= np.array([-2,2,-1])
 
 
 # Calculate the normal vector of the plane
-v1 = p3 - p1
-v2 = p2 - p1
+v1 = C - A
+v2 = B - A
 normal = np.cross(v1, v2)
 
 
 
 # Define the coefficients matrix and the constants vector
-A = np.array([[1, 1,-2], [1, 2,2],[0,1,-1]])
-B = np.array([1, 1,1])
+a1 = np.array([[1, 1,-2], [1, 2,2],[0,1,-1]])
+a2 = np.array([1, 1,1])
 
 # Solve the system of linear equations
-X = np.linalg.solve(A, B)
+X = np.linalg.solve(a1, a2)
 # Print the solution vector
 print(X)
 print('the equation of the plane is given by: ' )
 print("{}x  = 1".format(X))
 # Define the plane function
 def plane(x, y):
-    return (-normal[0] * x - normal[1] * y - np.dot(normal, p1)) * 1. / normal[2]
+    return (-normal[0] * x - normal[1] * y - np.dot(normal, A)) * 1. / normal[2]
 
 # Generate the meshgrid
 xx, yy = np.meshgrid(range(-10, 11), range(-10, 11))
@@ -39,7 +39,7 @@ z = plane(xx, yy)
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 ax.plot_surface(xx, yy, z)
-ax.scatter(p1[0], p1[1], p1[2], color='red')
-ax.scatter(p2[0], p2[1], p2[2], color='red')
-ax.scatter(p3[0], p3[1], p3[2], color='red')
+ax.scatter(A[0], A[1], A[2], color='red')
+ax.scatter(B[0], B[1], B[2], color='red')
+ax.scatter(C[0], C[1], C[2], color='red')
 plt.show()
