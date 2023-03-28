cc91cfb2e7bd722db2ddcd3bae23b5bf5ff94915
Shristy Sharma <121787965+ShristySharma@users.noreply.github.com>
2023-03-28 12:48:27 +0530

    Add files via upload

--- 9.10.5.11/codes/9.10.5.11.py
+++ 9.10.5.11/codes/9.10.5.11.py
@@ -1,43 +1,48 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import sys
-sys.path.insert(0,'/home/shri/CoordGeo')
-
-from line.funcs import *
-from triangle.funcs import *
-from conics.funcs import *
-
-a = 1
-A = np.array([[-1*a],[0]])
-C = np.array([[a],[0]])
-O = (A+C)/2
-theta_1 = 0.6*np.pi# np.random.rand()
-B = a*np.array([[np.cos(theta_1)],[np.sin(theta_1)]])
-theta_2 = -0.45*np.pi #np.random.rand()
-D = a*np.array([[np.cos(theta_2)],[np.sin(theta_2)]])
-cos1 = (C-A).T @ (D-A)/ (np.linalg.norm(C-A)*np.linalg.norm(D-A))
-cos2 = (C-B).T @ (D-B)/ (np.linalg.norm(C-B)*np.linalg.norm(D-B))
-print(f"Error = {cos1 - cos2}")
-#plots
-plt.figure(figsize=(10,10))
-c1 = circ_gen(O.T,a)
+from mpl_toolkits.mplot3d import Axes3D 
+#Generate line points
+def line_gen(A,B):
+  len =10
+  dim = A.shape[0]
+  x_AB = np.zeros((dim,len))
+  lam_1 = np.linspace(0,1,len)
+  for i in range(len):
+    temp1 = A + lam_1[i]*(B-A)
+    x_AB[:,i]= temp1.T
+  return x_AB
+# Define the coordinates of the points in the triangles
+A = np.array([0, 0])
+B = np.array([2, 0])
+C = np.array([2, 3])
+D = np.array([0, 3])
+#Generate lines
 l_AB = line_gen(A,B)
 l_BC = line_gen(B,C)
-l_AC = line_gen(A,C)
-l_AD = line_gen(A,D)
 l_CD = line_gen(C,D)
-plt.plot(l_AB[0],l_AB[1])
-plt.plot(l_BC[0],l_BC[1])
-plt.plot(l_AC[0],l_AC[1])
-plt.plot(l_AD[0],l_AD[1])
-plt.plot(l_CD[0],l_CD[1])
-plt.plot(c1[0],c1[1])
+l_DA = line_gen(D,A)
+l_AC = line_gen(A,C)
+# Plot the triangles
+plt.plot([A[0], B[0]], [A[1], B[1]], 'b')
+plt.plot([A[0], D[0]], [A[1], D[1]], 'b')
+plt.plot([C[0], B[0]], [C[1], B[1]], 'b')
+plt.plot([C[0], D[0]], [C[1], D[1]], 'b')
+plt.plot([A[0], C[0]], [A[1], C[1]], 'b')
 
-points = np.hstack((A,B,C,D,O))
-
-labels = np.array(['A','B','C','D','O'])
-
-for i in range(5):
-      plt.text(points[0][i]*1.05,points[1][i]*1.05,labels[i])
-plt.scatter(points[0],points[1])
+# Plot the points
+plt.plot(A[0], A[1], 'bo')
+plt.plot(B[0], B[1], 'bo')
+plt.plot(C[0], C[1], 'bo')
+plt.plot(D[0], D[1], 'bo')
+plt.plot(A[0], C[1], 'bo')
+# Label the points
+plt.text(A[0]-0.2, A[1]-0.2, 'A', fontsize=12)
+plt.text(B[0]+0.1, B[1]-0.2, 'B', fontsize=12)
+plt.text(C[0]+0.1, C[1]+0.1, 'C', fontsize=12)
+plt.text(D[0]-0.2, D[1]+0.1, 'D', fontsize=12)
 plt.show()
+plt.grid()
+plt.tight_layout()
+plt.savefig('fig_1.png')
+#plt.savefig('../figs/Figure1.png')
+
